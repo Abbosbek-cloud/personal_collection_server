523a812dd91100508a540ec70413f357dcef875c
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-10-05 01:05:07 +0500

    collection open api completed

--- api/v1/controllers/collections.js
+++ api/v1/controllers/collections.js
@@ -1,9 +1,33 @@
+const Collection = require("../../../models/Collection");
+
 async function getUserCollection(req, res) {
   // get a user's collection
+  try {
+    const { id } = req.params;
+    const userCollections = await Collection.find({ userId: id });
+    return res.status(200).send(userCollections);
+  } catch (error) {
+    return res.status(400).send({ message: "Error occured!" });
+  }
 }
 
 async function getAllCollections(req, res) {
   // get all collecttions
+  try {
+    const collections = await Collection.find({});
+    return res.status(200).send(collections);
+  } catch (error) {
+    return res.status(400).send({ message: "Error occured!" });
+  }
 }
 
-module.exports = { getUserCollection, getAllCollections };
+async function getLatestCollections(req, res) {
+  try {
+    const latestCollections = await Collection.find().sort({ createdAt: 1 });
+    return res.status(200).send(latestCollections);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
+
+module.exports = { getUserCollection, getAllCollections, getLatestCollections };
